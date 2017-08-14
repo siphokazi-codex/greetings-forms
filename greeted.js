ec4afc847ca74cc36e915b0b0597259dff127073
siphokazi-codex <aplenis@webmail.co.za>
2017-08-14 18:09:06 +0200

    Updated changes, so my greetings webapp greets using different languages

--- greeted.js
+++ greeted.js
@@ -2,32 +2,77 @@ module.exports = function(){
 
   const greetedList = [];
   const counterMap = {};
+  const nameList = [];
+//  const nameCounter = [];
 
   const getForm = function(req, res){
     res.render('greetings/index');
   };
 
+  const addScreen = function(req, res) {
+    res.render('greetings/add', {greetings: greetedList});
+  }
+
   const add = function(req, res){
 
   var username = req.body.username;
+  var languange = req.body.languange;
+  //var counterList[i]nameList
 
   var foundName = greetedList.find(function(currentName){
+
     return currentName === username;
   });
 
   if (!username) {
     req.flash('error', 'Name should not be blank');
   }
+  else if (!languange){
+    req.flash('error', 'Select languange to be greeted in');
+  }
   else {
-    if(!foundName){
-      greetedList.push(username);
-      //res.render('greetings/index', {message: 'Hello, ' + username});
-      req.flash('greetMessage', 'Hello, ');
+    if (!foundName) {
+      if (languange === 'english'){
+        req.flash('greetMessage', 'Hello, ' + username);
+        greetedList.push(username)
+      }
+      else if (languange === 'french') {
+        req.flash('greetMessage', 'Bonjour, ' + username);
+        greetedList.push(username);
+      }
+      else if (languange === 'tshivenda') {
+        req.flash('greetMessage', 'Ndaaa, ' + username);
+        greetedList.push(username);
+      }
     }
-    else {
-      req.flash('error', 'Welcome back, ' + username);
+    else if (foundName){
+      if (languange === 'english'){
+        req.flash('greetMessage', 'Hello, ' + username);
+      }
+      else if (languange === 'french') {
+          req.flash('greetMessage', 'Bonjour, ' + username);
+      }
+      else if (languange === 'tshivenda') {
+          req.flash('greetMessage', 'Ndaaa, ' + username);
+      }
     }
   }
+      /**(!foundName && languange === 'english'){
+      req.flash('greetMessage', 'Hello, ' + username);
+      greetedList.push(username);
+    }
+    else if (!foundName && languange === 'french') {
+      req.flash('greetMessage', 'Bonjour, ' + username);
+      greetedList.push(username);
+    }
+    else if (!foundName && languange === 'tshivenda') {
+      req.flash('greetMessage', 'Ndaaa, ' + username);
+      greetedList.push(username);
+    }
+  }**/
+//  greetedList.push(username);
+
+  var nameCounter = greetedList.length;
 
   if(counterMap[username] === undefined){
       counterMap[username] = 0;
@@ -35,22 +80,21 @@ module.exports = function(){
     counterMap[username] ++;
     const greetedCounter = counterMap[username]
 
-    res.render('greetings/index', {greetings : username});
-    console.log(username);
-  }
+    res.render('greetings/index', {counter : nameCounter});
+}
 
   const counter = function(req,res){
 
-  var username = req.parms.username;
+  var username = req.params.username
   const greetedCounter = counterMap[username];
   res.send("Hello,"+ " " + username + " " +"has been greeted" +" " + greetedCounter +" "+"time(s)")
-  console.log(greetedCounter);
     }
 
   return {
 
     getForm,
     add,
+    addScreen,
     counter
   }
 }
--- index.js
+++ index.js
@@ -23,12 +23,15 @@ app.use(bodyParser.json())
 app.use(session({ secret: 'keyboard cat', cookie: { maxAge: 60000 * 30 }}));
 app.use(flash());
 
-app.get('/greetings/add', greetedRoutes.add);
 app.get('/greeted', greetedRoutes.getForm);
 app.post('/greeted', greetedRoutes.add);
 
-app.get('/counter/:username', greetedRoutes.counter);
-app.get('/counter', greetedRoutes.counter);
+app.get('/greetings/add', greetedRoutes.addScreen);
+app.get('/greetings/index', greetedRoutes.add);
+app.post('/greetings/add', greetedRoutes.add)
+
+app.get('/greetings/counter/:username', greetedRoutes.counter);
+app.get('/greetings/counter', greetedRoutes.counter);
 
 //start the server
 var server = app.listen(3000, function(){
